579f8dbaf31db2456379f6ece9200eac74fa4a84
Shin-002 <anko13ko@yahoo.co.jp>
2020-06-15 05:37:59 +0900

    create view,serializer setting url

--- core/admin.py
+++ core/admin.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from core import models
-# Register your models here.
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email']
     fieldsets = (
-        (None, {'fields': ('email', 'password',)}),
-        (_('personal Info'), {'fields': ('',)}),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('',)}),
         (
             _('Permissions'),
             {
@@ -24,10 +23,13 @@ class UserAdmin(BaseUserAdmin):
     )
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2',)
+            'classes':('wide',),
+            'fields':('email','password1','password2')
         }),
     )
+
+
+
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Profile)
 admin.site.register(models.Message)
--- core/models.py
+++ core/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
-
 def upload_path(instance, filename):
     ext = filename.split('.')[-1]
-    return '/'.join(['image'], str(instance.userPro.id)+str(instance.nickName)+str(".")+str(ext))
+    return '/'.join(['image', str(instance.userPro.id)+str(instance.nickName)+str(".")+str(ext)])
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -23,14 +23,14 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using= self._db)
 
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=50, unique=True)
-    is_activate = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
@@ -46,7 +46,7 @@ class Profile(models.Model):
         settings.AUTH_USER_MODEL, related_name='userPro',
         on_delete=models.CASCADE
     )
-    create_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     img = models.ImageField(blank=True, null=True, upload_to=upload_path)
 
     def __str__(self):
@@ -54,17 +54,17 @@ class Profile(models.Model):
 
 class FriendRequest(models.Model):
     askFrom = models.ForeignKey(
-         settings.AUTH_USER_MODEL, related_name='askFrom',
+        settings.AUTH_USER_MODEL, related_name='askFrom',
         on_delete=models.CASCADE
     )
     askTo = models.ForeignKey(
-         settings.AUTH_USER_MODEL, related_name='askTo',
+        settings.AUTH_USER_MODEL, related_name='askTo',
         on_delete=models.CASCADE
     )
     approved = models.BooleanField(default=False)
 
-class Meta:
-    unique_together = (('askFrom', 'askTo'),)
+    class Meta:
+        unique_together = (('askFrom', 'askTo'),)
 
     def __str__(self):
         return str(self.askFrom) + '----->' + str(self.askTo)
@@ -72,14 +72,14 @@ class Meta:
 class Message(models.Model):
 
     message = models.CharField(max_length=140)
-    sender =  models.ForeignKey(
-         settings.AUTH_USER_MODEL, related_name='sender',
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='sender',
         on_delete=models.CASCADE
     )
-    receiver =  models.ForeignKey(
-         settings.AUTH_USER_MODEL, related_name='receiver',
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='receiver',
         on_delete=models.CASCADE
     )
 
     def __str__(self):
-        return self.sender
+        return str(self.sender)
